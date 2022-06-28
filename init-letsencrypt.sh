c633508a6a03da82666e2ee72521af2a8079dfbe
Edison Pizarro <edipizarro@uc.cl>
2022-06-27 22:07:47 -0400

    letsencrypt

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 
-if ! [ -x "$(command -v docker compose)" ]; then
-  echo 'Error: docker compose is not installed.' >&2
-  exit 1
-fi
-
 domains=(api.iic2173-g19.xyz www.api.iic2173-g19.xyz flower.iic2173-g19.xyz www.flower.iic2173-g19.xyz auth.iic2173-g19.xyz www.auth.iic2173-g19.xyz)
 rsa_key_size=4096
 data_path="./config/certbot"
 email="edipizarro@uc.cl" # Adding a valid address is strongly recommended
-staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
+staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
